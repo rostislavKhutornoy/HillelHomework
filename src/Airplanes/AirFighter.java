cc53773feca3cc4346fa92a4a95bb683597a9006
rostislavKhutornoy <ichunki79@gmail.com>
2022-05-09 02:43:06 +0300

    Seventh Homework

---
+++ src/Airplanes/AirFighter.java
@@ -0,0 +1,15 @@
+package Airplanes;
+
+public class AirFighter extends Airplane{
+
+    public AirFighter(String type, String model) {
+        super(type, model);
+    }
+
+    @Override
+    public void printAirplaneInfo() {
+        System.out.println("Aircraft type: " + type +
+                ", model: " + model);
+    }
+
+}
---
+++ src/Airplanes/BusAirplane.java
@@ -0,0 +1,17 @@
+package Airplanes;
+
+public class BusAirplane extends Airplane{
+
+    public BusAirplane(String type, String model, int ecoPlaces, int busPlaces) {
+        super(type, model, ecoPlaces, busPlaces);
+    }
+
+    @Override
+    public void printAirplaneInfo() {
+        System.out.println("Aircraft type: " + type +
+                ", model: " + model +
+                ", num. of eco. class passengers: " + ecoPlaces +
+                ", business class passengers: " + busPlaces);
+    }
+
+}
